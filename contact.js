6e92cf9faa3c05382ab648209da9bc6859630002
A.leroch <alexislerocheleuil@gmail.com>
2025-02-19 15:57:18 +0000

    Ajout du contact pour que je reçoive les message par mails

---
+++ contact.js
@@ -0,0 +1,18 @@
+import emailjs from '@emailjs/browser';
+
+function sendEmail(e) {
+    e.preventDefault(); // Empêche le rechargement de la page lors de l'envoi du formulaire
+
+    // Envoi du formulaire à EmailJS avec le service, le template et ta clé API
+    emailjs.sendForm('service_h3md7bp', 'template_hu5s7fs', e.target, 'jXg14OMTd1NcqRSSF')
+      .then((result) => {
+          console.log(result.text);
+          alert("Votre message a été envoyé !");
+      }, (error) => {
+          console.log(error.text);
+          alert("Une erreur est survenue. Veuillez réessayer.");
+      });
+}
+
+// Initialisation d'EmailJS avec ta clé publique
+emailjs.init('jXg14OMTd1NcqRSSF');
