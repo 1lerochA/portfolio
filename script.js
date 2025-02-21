4441f3f691a43ffd5d053213bb66d593a85f99dc
A.leroch <alexislerocheleuil@gmail.com>
2025-02-21 13:49:59 +0000

    test script

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav ul li a [href*=' + id + ' ]').classList.add(active)
+            })
+        }
+    })
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const menuIcon = document.querySelector('.menu-icon');
     const navbar = document.querySelector('.navbar');
@@ -33,7 +55,6 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     };
-});
 
     // Initialisation d'EmailJS
     emailjs.init('jXg14OMTd1NcqRSSF'); // Remplace par ton propre ID EmailJS
